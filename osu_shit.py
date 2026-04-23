412cc4ac9ea3390815a82be5cedc0ffff0442ba8
Michael Zhang <mail@mzhang.io>
2026-04-23 02:15:27 -0400

    add osu file parser

--- osu_shit.py
+++ osu_shit.py
@@ -1,3 +1,6 @@
+import rosu_pp_py as rosu
+import libosu
+
 MOD_MULTIPLIERS = {
     "NF": 1.0,
     "EZ": 0.5,
@@ -15,3 +18,42 @@ def parse_osu_mods(
     data,
 ):  # data is the json object from the osu beatmap table data column
     print("hello")
+
+
+def get_beatmap_info(path: str):
+    with open(path, "r") as f:
+        bb = f.read()
+    b = rosu.Beatmap(content=bb)
+    p = rosu.Performance()
+    pp = p.calculate(b)
+    map = libosu.parse_beatmap(bb)
+    print(dict(
+        mode=b.mode,
+        ar=b.ar,
+        cs=b.cs,
+        hp=b.hp,
+        od=b.od,
+        bpm=b.bpm,
+        n_circles=b.n_circles,
+        n_objects=b.n_objects,
+        n_sliders=b.n_sliders,
+        n_spinners=b.n_spinners,
+        slider_tick_rate=b.slider_tick_rate,
+        slider_multiplier=b.slider_multiplier,
+        artist=map.artist,
+        artist_unicode=map.artist_unicode,
+        title=map.title,
+        title_unicode=map.title_unicode,
+        difficulty_name=map.difficulty_name,
+        # pp_aim=pp.pp_aim,
+        # pp_speed=pp.pp_speed,
+        # pp_flashlight=pp.pp_flashlight,
+        # pp_accuracy=pp.pp_accuracy,
+        # effective_miss_count=pp.effective_miss_count,
+        difficulty=pp.difficulty.reading,
+    ))
+
+
+if __name__ == "__main__":
+    # example
+    get_beatmap_info("source-data/2026_01_01_osu_files/4030010.osu")
